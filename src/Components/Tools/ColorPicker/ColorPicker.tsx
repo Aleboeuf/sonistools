e2a80df548401e56b8b1f469bbb1bef50c2cafc7
Alexis Leboeuf <alexis.leboeuf77@gmail.com>
2022-08-14 15:03:45 +0200

    4 - Ajout de l'outil ColorPicker

--- src/Components/Tools/ColorPicker/ColorPicker.tsx
+++ src/Components/Tools/ColorPicker/ColorPicker.tsx
@@ -1,7 +1,27 @@
+import { ColorPicker as ColorPalette, useColor } from "react-color-palette";
+import "react-color-palette/lib/css/styles.css";
+import "./ColorPicker.css";
+
 function ColorPicker() {
+    const [color, setColor] = useColor("hex", "#121212");
+
     return (
         <main>
-            <h2>ColorPicker</h2>
+            <div className="container">
+                <div className="header">
+                    <div className="title">ColorPicker</div>
+                </div>
+                <div className="content">
+                    <ColorPalette
+                        width={456}
+                        height={228}
+                        color={color}
+                        onChange={setColor}
+                        hideHSV
+                        dark
+                    />
+                </div>
+            </div>
         </main>
     );
 }
